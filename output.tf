7964229a23f3bea8bed735c4e3fcda3ed8e475e3
Stefan Moravcik <stefan@virsas.com>
2023-05-25 21:39:06 +0200

    removed ai outputs

--- output.tf
+++ output.tf
@@ -22,22 +22,3 @@ output "etag" {
 output "hosted_zone_id" {
   value = aws_cloudfront_distribution.vss.hosted_zone_id
 }
-
-output "oai_id" {
-  value = try(aws_cloudfront_origin_access_identity.vss.id, "")
-}
-output "oai_etag" {
-  value = try(aws_cloudfront_origin_access_identity.vss.etag, "")
-}
-output "oai_cloudfront_access_identity_path" {
-  value = try(aws_cloudfront_origin_access_identity.vss.cloudfront_access_identity_path, "")
-}
-output "oai_iam_arn" {
-  value = try(aws_cloudfront_origin_access_identity.vss.iam_arn, "")
-}
-output "oac_id" {
-  value = try(aws_cloudfront_origin_access_control.vss.id, "")
-}
-output "oac_etag" {
-  value = try(aws_cloudfront_origin_access_control.vss.etag, "")
-}
